670d84ede97b7ec840f6cbc02c0f877ee1355961
adorableco <tpdusdl1001@knu.ac.kr>
2023-11-21 13:38:58 +0900

    Add theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ PATH = 'content'
 TIMEZONE = 'Asia/Seoul'
 
 DEFAULT_LANG = 'ko'
-
+THEME = 'theme/aboutwilson'
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -28,4 +28,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
